e904c8a6cecf1f2ef01e4b935cd18279789fef29
Oleksandr Poltavets <sancho.ck@gmail.com>
2015-03-08 17:55:00 +0200

    users-auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
-  get 'gardens/index'
 
-  get 'gardens/show'
+  get 'signup', to: 'users#new', as: :signup
+  get 'login', to: 'sessions#new', as: :login
+  delete 'logout', to: 'sessions#destroy', as: :logout
+
+  resources :users
+  resources :sessions
+  resources :gardens
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'gardens#index'
+  # root 'gardens#index'
+  root 'sessions#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
