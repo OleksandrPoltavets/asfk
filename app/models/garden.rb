47d64922d4138a3dbf241e3ed4fce622bc5e7726
Oleksandr Poltavets <sancho.ck@gmail.com>
2015-03-08 22:58:11 +0200

    user-relations

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,2 +1,4 @@
 class Garden < ActiveRecord::Base
+  validates_uniqueness_of :number, :name
+  has_many :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308141633) do
+ActiveRecord::Schema.define(version: 20150308202444) do
 
   create_table "gardens", force: :cascade do |t|
     t.integer  "number"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150308141633) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "garden_id"
   end
 
 end
